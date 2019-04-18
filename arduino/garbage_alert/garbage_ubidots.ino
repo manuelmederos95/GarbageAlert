e5366976c8cbb16fe9f646f19f04ebb035330f46
manuel <manuel.mederos95@gmail.com>
2019-04-18 18:59:45 +0200

    ubidots commit

--- arduino/garbage_alert/garbage_ubidots.ino
+++ arduino/garbage_alert/garbage_ubidots.ino
@@ -1,27 +1,17 @@
-#include "UbidotsMicroESP8266.h"
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
-
+#include "UbidotsMicroESP8266.h"  
 
 #define TOKEN  "BBFF-EOWLPFC2CqXVz6olPpcMMN1VTydEqU"  // Put here your Ubidots TOKEN
 #define WIFISSID "ESGI" // Put here your Wi-Fi SSID
 #define PASSWORD "Reseau-GES" // Put here your Wi-Fi password
 #define maxBlockedTime 3000
 #define idPoubelle "5cb73e2c1d847228b8ab8ac3" //Ici id de poubelle
-#define BLYNK_PRINT Serial
-#define nomPoubelle "POUBELLE2" //name in blink notification 
 
-// You should get Auth Token in the Blynk App.
-char auth[] = "9965480b40994ffbb077346f70c5e632";
 const int led_green = D0;
 const int led_red = D1;
+
 Ubidots client(TOKEN);
 int counter = 0;
 bool isSend = false;
-int pinValue; //reset pinValue
-float pinRead0; //photoreceptor read value
-WidgetLED led1(V0); //warning led in application
-
 
 int i = 1;
 
@@ -32,64 +22,39 @@ void setup() {
 
   pinMode(led_green, OUTPUT); // ext. led
   pinMode(led_red, OUTPUT); // ext. led
-
+  
   pinMode(D7, OUTPUT);
   pinMode(A0, INPUT);
-  digitalWrite(D7, 1);
+  digitalWrite(D7,1);
   digitalWrite(led_red, 0);
   client.wifiConnection(WIFISSID, PASSWORD); // Setting wifi connection with ssid and password
   client.add(idPoubelle, 0);
   client.sendAll(false);
-
-  Blynk.begin(auth, WIFISSID, PASSWORD);
-  
-}
-
-
-
-// This function will be called in loop
-// Every time Reset button is pushed it will reset counter and turn off warning led in app
-BLYNK_WRITE(V1)
-{
-  pinValue = param.asInt();
-  if (pinValue == 1) {
-    counter = 0;
-    led1.off();
-  }
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  //execute Blynk function
-  Blynk.run();
-
   // read the input on pin D8:
-  pinRead0 = analogRead(A0);
+  float pinRead0 = analogRead(A0);
   Serial.print(pinRead0);
   if (pinRead0 < 500) {
     counter++;
     Serial.print(" Blocked ");
     Serial.println(counter);
-    if (counter == maxBlockedTime) {
+    if(counter == maxBlockedTime && isSend == false) {
       digitalWrite(led_red, 1);
       Serial.println("EVENT DECLANCHER");
-      char mess[] = " IS FULL"; 
-      char * fullalert = (char *) malloc(1 + strlen(nomPoubelle)+ strlen(mess) );
-      strcpy(fullalert, nomPoubelle);
-      strcat(fullalert, mess);
-      Serial.println(fullalert);
-      Blynk.notify(fullalert);
-      led1.on();
+      counter = 0;
+      isSend = true;
       client.add(idPoubelle, 1);
       client.sendAll(false);
     }
-  } else {
+  }else {
     Serial.print(" Pass ");
     Serial.println(counter);
     digitalWrite(led_green, 1);
     delay(100);
     digitalWrite(led_green, 0);
     counter = 0;
-    led1.off();
   }
 }
